d606b939ec7bb997c2cfff6db7c792307e86b3a9
Pamela Fracassa <pamela.fracassa@spindox.it>
2018-04-27 19:56:45 +0200

    optimized images

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
- 
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 // Lets bring es6 to es5 with this.
 // Babel - converts ES6 code to ES5 - however it doesn't handle imports.
 // Browserify - crawls your code for dependencies and packages them up 
@@ -24,7 +25,16 @@ gulp.task('es6', function() {
     .pipe(source('bundle.js'))
     .pipe(gulp.dest(''));
 });
- 
+
+gulp.task('images-process', function() {
+  return gulp.src('images/*')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
+
 gulp.task('watch',function() {
 	gulp.watch('**/*.js',['es6'])
 });
